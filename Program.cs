d1b0347d14f844cdcc0bb40ede08802b4cd8ec49
semeniuc97@gmail.com <essien15>
2019-06-16 16:25:19 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GitVerifiedCommitTrying
+{
+    class Person
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (value.Length <= 6)
+                {
+                    this.name = value + " " + "additional";
+                }
+            }
+        }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            var person = new Person();
+            person.Name = name;
+
+            Console.WriteLine(person.Name);
+        }
+    }
+}
